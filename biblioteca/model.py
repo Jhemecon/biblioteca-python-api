500ab0a5a1888018917f570f7a0623cd7c6bfe66
JHEMESON DE OLIVEIRA CONDE <125128480+Jhemecon@users.noreply.github.com>
2026-05-08 09:08:38 -0400

    Add Livro and Usuario classes for library system

---
+++ biblioteca/model.py
@@ -0,0 +1,39 @@
+class Livro:
+    def __init__(self, titulo, autor, isbn):
+        self.titulo = titulo
+        self.autor = autor
+        self.isbn = isbn
+        self.__disponivel = True
+
+    def esta_disponivel(self):
+        return self.__disponivel
+
+    def emprestar(self):
+        if self.__disponivel:
+            self.__disponivel = False
+            return True
+        return False
+
+    def devolver(self):
+        self.__disponivel = True
+
+    def to_dict(self):
+        return {
+            "titulo": self.titulo,
+            "autor": self.autor,
+            "isbn": self.isbn,
+            "disponivel": self.__disponivel
+        }
+
+
+class Usuario:
+    def __init__(self, nome, id_usuario):
+        self.nome = nome
+        self.id_usuario = id_usuario
+        self.livros_emprestados = []
+
+    def pegar_emprestado(self, livro):
+        if livro.emprestar():
+            self.livros_emprestados.append(livro)
+            return True
+        return False
